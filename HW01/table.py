e4fec07936f444380d6118955eecdb5195b69578
Giacomo Ciro <giacomociro02@gmail.com>
2024-09-14 19:14:06 +0200

    grinding

--- HW01/table.py
+++ HW01/table.py
@@ -2,19 +2,13 @@ import sys
 import os
 import re
 
-class Processor():
+class Compiler():
     
     def __init__(self,
-                 ignore: list=None,
-                 words: list=None,
-                 blanks: list=None,
-                 keywords: list=None
-                 )->None:
-        self.ignore = ignore
-        self.words = words
-        self.blanks = blanks
-        self.keywords = keywords
+                 )->None:    
         
+        self.vars = []
+        self.ids = []
         return
     
     def tokenize(self,
@@ -69,7 +63,7 @@ class Processor():
         )->None:
         """
         
-        Parses the input according to parsing rules. Yields Variable Declarations, Assignments and Show Instructions.
+        Parses the input according to parsing rules. Returns Variable Declarations, Assignments and Show Instructions.
 
             tokens:
                 <list>:
@@ -77,10 +71,78 @@ class Processor():
         Returns
 
         """
+        # Divide instructions
+        instructions = []
+        buffer = []
+        for t in tokens:
+            if t == ';':
+                instructions.append(buffer)
+                buffer = []
+            else:
+                buffer.append(t)
+        
+        # Process instructions
+        for i in instructions:
+
+            # DECLARATION
+            if i[0] == 'var':
+                print(f'Declaration: {i}')
+                for t in i[1:]:
+                    if t == ';':
+                        break
+                    self.vars.append(t)
+            
+            # ASSIGNMENT
+            elif i[1] == '=':
+                print(f'Assignment: {i}')
+                if (i[0] not in self.vars) and (i[0] not in self.ids):
+                    buffer = []
+                    for t in i[2:]:
+                        if t == ';':
+                            break
+                        buffer.append(t)
+                    self.ids.append((i[0], buffer))         # e.g. ('z', ['x', 'and', 'y'])
+                else:
+                    raise Exception(f"{i[0]} already exists.")
+            
+
+            # SHOW
+            elif i[0] == 'show' or i[0] == 'show_ones':
+                print(f'Show: {i}')
+                # TODO: when a show instruction is called do the following:
+                # evaluate ALL ids (not only the ones to be showed, there might be cross references)
+                # show the required ones
+
+            # INVALID
+            else:
+                raise Exception(f"{i} is an invalid instruction")
         
         pass
 
-processor = Processor()
+    def _solve():
+        """
+
+        Solves all the ids in self.ids
+        
+        """
+
+        # TODO: the value of the vars are binary numbers increasing at each row
+        # x y z
+        # 0 0 0
+        # 0 0 1
+        # 0 1 0
+        # etc. 
+        # so you have a clean way to define raws, just a n bit integers (where n = len(self.vars)) 
+        # and the identifiers are obtained as a consequence
+    
+    def _show(ids):
+        """
+        
+        Shows the truth table for a list of identifiers.
+        
+        """
+
+compiler = Compiler()
 
 with open(sys.argv[1], 'r') as f:
     
@@ -91,9 +153,17 @@ with open(sys.argv[1], 'r') as f:
     print("-"*30)
     print(f)
     
-    f = processor.tokenize(f)
+    tokens = compiler.tokenize(f)
 
     print("-"*30)
     print(f'Tokenized Input:')
     print("-"*30)
-    print(f)
+    print(tokens)
+
+    print("-"*30)
+    print(f'Instructions:')
+    print("-"*30)
+
+    compiler.parse(tokens)
+    print(compiler.vars)
+    print(compiler.ids)
