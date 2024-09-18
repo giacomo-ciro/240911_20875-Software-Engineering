4ac882ec9eb266b9cdeb26b1cf43603c7d9dbcaa
Giacomo Ciro <giacomociro02@gmail.com>
2024-09-18 10:44:06 +0200

    add ta2

--- TA02/objcode.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int the_number = -1;
-
-int main()
-{
-	scanf("%d", &the_number);
-	return 0;
-}
--- TA02/optimize.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	int r = 0;
-
-	for (int i = 0; i < 1000000; i++)
-		r = r + 2;
-	
-	return r;
-}
